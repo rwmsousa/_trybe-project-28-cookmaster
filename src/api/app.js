2e5a2ead7c6b02f21e6bb25e8f75dbfeb631e239
ricardo-sousa <rwmsousa@gmail.com>
2022-01-17 14:35:23 -0300

    open PR initial

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const errorMiddleware = require('../middlewares/errorHandler');
 
 const app = express();
+const router = require('../routes');
+
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
-  response.send();
+    response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use(express.json());
+app.use(router);
+app.use(errorMiddleware);
 
 module.exports = app;
