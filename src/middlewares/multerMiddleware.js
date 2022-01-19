36128f2b963bf63565e336ee1ffa5afa5b00cfe6
ricardo-sousa <rwmsousa@gmail.com>
2022-01-19 20:03:16 -0300

    requirement 10

--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) =>
-        callback(null, path.resolve(__dirname, '..', '..', 'uploads')),
-    filename: (req, file, callback) => callback(null, `${req.params.id}.jpeg`),
+        callback(null, path.resolve(__dirname, '..', 'uploads')),
+    filename: (req, file, callback) => callback(null, `${ req.params.id }.jpeg`),
     // filename: (req, file, callback) => callback(null, `${ Date.now() } - ${ file.originalname }`)
 });
 
