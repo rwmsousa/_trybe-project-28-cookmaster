81bf1625f6f44264eb05fda009678a4a6f5d861d
ricardo-sousa <rwmsousa@gmail.com>
2022-01-25 13:53:24 -0300

    adjust of linter

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,5 @@
 const app = require('./app'); // import app from app.js
 
-const PORT  = process.env.PORT || 3000; // import PORT from .env file
+const PORT = process.env.PORT || 3000; // import PORT from .env file
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`)); // listen on PORT or 3000
